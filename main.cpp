11fd428d19e502f836fe07f9ca9aa43bc819a1cc
odomar <severingosset@orange.fr>
2020-01-25 15:51:57 +0100

    added regex for first few tests

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include "iostream"
+#include <string>
+#include <regex>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	cout << argv[1] << endl;
+	regex reg("\\(\\d*;\\d*\\)");
+	vector<string> vec;
+	smatch res;
+	string str(argv[1]);
+
+	while(regex_search (str, res, reg)){
+		vec.push_back(res[0].str());
+		str = res.suffix();
+	}
+	if (vec.size() < 3) {
+		cout << argv[1] << endl;
+	}
+	else {
+		cout << vec[0] << " " << vec[2] << endl;
+	}
+
+	
 	return 0;
 }
 
